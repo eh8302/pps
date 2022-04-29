00d8f525a22255f09648e0a96687d7b85a4cac4d
eh8302 <103807710+eh8302@users.noreply.github.com>
2022-04-30 00:02:54 +0530

    Create clap1.c

---
+++ clap1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int a,b,result,choice;
+    printf("Enter first number\n");
+    scanf("%d",&a);
+    printf("Enter the second number\n");
+    scanf("%d",&b);
+    printf("Enter 1 for addition or 2 for multiplication\n");
+    scanf("%d",&choice);
+    result = (choice==1)?a+b:(choice==2)?a*b:printf("invalid");
+    if (choice==1 || choice==2)
+    printf("the result is %d \n",result);
+    return 0;
+}
